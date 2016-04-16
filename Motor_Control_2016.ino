abbffe6215be280b6745960fd54968f8249e28bc
SpaceInk <pjv7@case.edu>
2016-04-16 16:05:21 -0400

    Added files via upload

---
+++ Motor_Control_2016.ino
@@ -0,0 +1,43 @@
+//Link to setup page from instructables: Arduino Reed Switch
+// http://www.instructables.com/id/Arduino-Reed-Switch/?ALLSTEPS 
+
+
+const int pinSwitch = 12; //Pin Reed
+
+const int pinLed = 13; //Pin LED
+
+int StatoSwitch = 0;
+
+void setup()
+
+{
+
+pinMode(pinLed, OUTPUT); //Imposto i PIN
+
+pinMode(pinSwitch, INPUT);
+
+}
+
+void loop()
+
+{
+
+StatoSwitch = digitalRead(pinSwitch); //Leggo il valore del Reed
+
+if (StatoSwitch == HIGH)
+
+{
+
+digitalWrite(pinLed, HIGH);
+
+}
+
+else
+
+{
+
+digitalWrite(pinLed, LOW);
+
+}
+
+}
