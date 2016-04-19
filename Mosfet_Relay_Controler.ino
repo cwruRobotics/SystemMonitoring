7513062f179ac841b4c0a87b1ff8ff7a7c8d2b6b
adamlaw25 <jxl1305@case.edu>
2016-04-19 17:27:58 -0400

    Added files via upload

---
+++ Mosfet_Relay_Controler.ino
@@ -0,0 +1,34 @@
+//Link to setup page from instructables: Arduino Reed Switch
+// http://www.instructables.com/id/Arduino-Reed-Switch/?ALLSTEPS 
+
+
+const int pinSwitch = 12; //Pin Reed
+
+const int mosfet = 5; //Pin LED
+
+int pinVal = 0;
+
+void setup()
+
+{
+
+pinMode(mosfet, OUTPUT); //Imposto i PIN
+//Serial.begin(9600);
+pinMode(pinSwitch, INPUT);
+
+}
+
+void loop()
+
+{
+
+//pinVal = analogRead(pinSwitch); //Leggo il valore del Reed
+digitalWrite(pinSwitch, HIGH);
+pinVal = digitalRead(pinSwitch);
+if (pinVal == HIGH){
+  digitalWrite(mosfet, HIGH);
+}
+//Serial.print(pinVal);
+
+
+}
